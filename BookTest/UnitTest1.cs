8289c85276e4a3d5734abc75880241fa6f454638
Nithesh S <nitheshvilla@gmail.com>
2021-10-15 12:20:10 +0530

    unit testing added

--- BookTest/UnitTest1.cs
+++ BookTest/UnitTest1.cs
@@ -1,18 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using WebApplication.Controllers;
+using WebApplication.KaniniModel;
 
 namespace BookTest
 {
-    public class Tests
-    {
-        [SetUp]
+  
+        public class Tests
+        {
+            public Book c;
+            public BooksController mc;
+            public Logintabl lt;
+            [SetUp]
         public void Setup()
         {
+            c = new Book();
+            mc = new BooksController();
+            lt = new Logintabl();
         }
-
         [Test]
-        public void Test1()
+        public void ValidGetTest()
         {
-            Assert.Pass();
+
+
+            var res = mc.GetAllBooks().Result as OkObjectResult;
+            Assert.AreEqual(200, res.StatusCode);
         }
+        [Test]
+        public void InValidGetTest()
+        {
+
+
+            var res = mc.GetAllBooks().Result as OkObjectResult;
+            Assert.AreEqual(500, res.StatusCode);
+        }
+       
     }
 }
